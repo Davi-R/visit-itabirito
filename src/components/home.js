ebfaf5fb3c5e9bd22261e494d2d7ae079c9b1a9c
DaviRoro <davirpereirad@gmail.com>
2024-03-18 18:38:46 +0000

    project created

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <header>
+                <h1>Header</h1>
+            </header>
+            <h2>Title</h2>
+        </div>
+    );
+};
+
+export default Home;
